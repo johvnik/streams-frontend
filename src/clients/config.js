76633b332d79db3fdda791f534f7c2755442045b
John Vaught <john.vaught@onfractal.com>
2020-08-17 12:38:41 -0400

    general updates

--- src/clients/config.js
+++ src/clients/config.js
@@ -5,8 +5,6 @@ if (__NODE__) {
 	s3Config = {
 		storageBucket: 'streams-photo-storage',
 		zone: 'us-east-2',
-		accessKey: 'AKIAYY2TBF6MZYPGCA4U',
-		secretKey: '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',
 	}
 }
 
